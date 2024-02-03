6c9e5808b17f7a803b2d8061b6893f3e444089d8
efoneli <abbaefoneli@gmail.com>
2024-02-03 14:42:50 +0100

    server rounded up

---
+++ client/src/App.js
@@ -0,0 +1,16 @@
+import "./App.css";
+import { Fragment } from "react";
+import InputTodo from "./components/InputTodo";
+
+function App() {
+  return (
+    <Fragment>
+      <div className='container'>
+      <InputTodo />
+      </div>
+      
+    </Fragment>
+  );
+}
+
+export default App;
